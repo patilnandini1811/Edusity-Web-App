5b042313fc4fd75384f7d96203df8d044433de4a
patilnandini1811 <patilnandini1811@gmail.com>
2025-01-18 02:17:39 +0530

    Add Program component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
+import Program from './components/program/Program'
 
 function App() {
  
@@ -10,6 +11,7 @@ function App() {
       <div>
       <Navbar />
       <Hero/>
+      <Program/>
       </div>
       
   )
