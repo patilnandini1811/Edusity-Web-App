426fff61c3b9ddbf02240d5fa5a20ff5d92aac95
patilnandini1811 <patilnandini1811@gmail.com>
2025-01-22 21:24:55 +0530

    Add new components, implement core features, and refine CSS styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css'
+import About from './components/About/About'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Program from './components/program/Program'
+import Title from './components/Title/Title'
 
 function App() {
  
@@ -11,7 +13,12 @@ function App() {
       <div>
       <Navbar />
       <Hero/>
-      <Program/>
+      <div className='container'>
+      <Title subTitle='Our Programs' title='What we offer'/>
+     <Program/>
+     <About/>
+      
+      </div>
       </div>
       
   )
